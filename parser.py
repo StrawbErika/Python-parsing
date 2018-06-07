baf93770fa24d3a922b4849cc5765bf5c1c30d5d
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-06-07 14:45:12 +0800

    +parsed & paired question and answers

--- parser.py
+++ parser.py
@@ -1,7 +1,27 @@
 import urllib.request
+import re
+from bs4 import BeautifulSoup
+
 with urllib.request.urlopen('https://stackoverflow.com/questions/24458163/what-are-the-parameters-for-sklearns-score-function') as response:
    html = response.read()
 
-file = open("stackoverflow.txt","w") 
-file.write(str(html))
+soup = BeautifulSoup(html, 'html.parser')
+
+data = {}
+list_of_answers = []
+
+ 
+count = 0
+answer = soup.find_all("div", class_="answercell post-layout--right")
+while(count != len(answer)):
+    ans=answer[count].contents
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', str(ans[1].find_all("p")).strip("[]"))
+    list_of_answers.append(cleantext)
+    count=count+1
+
+data[str(soup.title.contents).strip("[]")] = list_of_answers
+
+file = open("answers.txt","w") 
+file.write(str(data))
 file.close() 
