b05c357d0d20b3d0bf9fab469ff6c69f16982b79
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-06-07 16:09:49 +0800

    + gets question authors, + gets question description

--- parser.py
+++ parser.py
@@ -2,6 +2,11 @@ import urllib.request
 import re
 from bs4 import BeautifulSoup
 
+def cleanhtml(raw_html):
+    clean = re.compile('<.*?>')
+    cleantxt = re.sub(clean, '', raw_html)
+    return cleantxt
+
 with urllib.request.urlopen('https://stackoverflow.com/questions/24458163/what-are-the-parameters-for-sklearns-score-function') as response:
    html = response.read()
 
@@ -10,18 +15,40 @@ soup = BeautifulSoup(html, 'html.parser')
 data = {}
 list_of_answers = []
 
- 
 count = 0
 answer = soup.find_all("div", class_="answercell post-layout--right")
 while(count != len(answer)):
     ans=answer[count].contents
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', str(ans[1].find_all("p")).strip("[]"))
-    list_of_answers.append(cleantext)
+    list_of_answers.append(cleanhtml(str(ans[1].find_all("p")).strip("[]")))
     count=count+1
 
-data[str(soup.title.contents).strip("[]")] = list_of_answers
+question_data = {}
+
+question = soup.find_all("div", class_="postcell post-layout--right")
+questions = str(soup.title.contents).strip("[]") 
+question_contents = question[0].contents
+description = cleanhtml(str(question_contents[1].find_all("p")).strip("[]"))
+
+question_authors = []
+author_content = question_contents[5].find_all("div", class_="user-details")
+i = 0
+while(i != len(author_content)):
+    auth=author_content[i].contents
+    question_authors.append(str(auth[1].contents).strip("[]"))
+    i=i+1
+
+
+question_data = {
+    "Question" : questions,
+    "Description" : description,
+    "Authors" : question_authors, 
+    # "Upvote" : upvote
+}
+
+
+data[str(question_data)] = list_of_answers
 
 file = open("answers.txt","w") 
-file.write(str(data))
+file.write(str(question))
 file.close() 
+
