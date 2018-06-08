f8b3d5d1c0cf918aefd3c0e4ee20f56edc7be290
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-06-08 13:28:52 +0800

    + write_to_file able to save from nothing

--- filewriter.py
+++ filewriter.py
@@ -1,11 +1,12 @@
 from tkinter import Tk
 from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
 
 def write_to_file(data):
     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    file_name = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    file_name = asksaveasfilename() # show an "Open" dialog box and return the path to the selected file
     print(file_name)
-    file = open(file_name,"w") 
+    file = open(file_name + ".json","w") 
     file.write(data)
     file.close() 
